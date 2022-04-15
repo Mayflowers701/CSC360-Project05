c0b1835f320bfdd08440ea9143b1e1c5dbe42b6f
Eli <elimadi701@gmail.com>
2022-04-15 18:13:20 -0500

    Outline in main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
 
-  cout << "test" << endl;
+  //command line input for number:
+  //if there is not exactly on argument, return
+  if(argc != 2){
+    cout << "This program accepts only one argument." << endl;
+    return 0;
+  }
+  //else, initialize string for input
+  string input = argv[1];
+
+
+  //call recursor on input, with endpoint 0 and inning 9
+  int result = -1;
+
+
+  //print result
+  cout << "Divisions: " << input << endl;
+  cout << "Result: " << result << endl;
 
   return 0;
 }
